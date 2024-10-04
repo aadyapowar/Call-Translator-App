5a9971227e5f0c34370fb45982eaf9cb000036c3
Adarsh Powar <164239665+BusyEngineer01@users.noreply.github.com>
2024-10-04 19:42:54 +0530

    Android Call Translator App

---
+++ app/src/main/java/com/techad/calltranslatorapp/LoginPage.kt
@@ -0,0 +1,28 @@
+package com.techad.calltranslatorapp
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class LoginPage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_login_page)
+
+        val signin_with_phone_number = findViewById<View>(R.id.signin_with_phone_number)
+
+        signin_with_phone_number.setOnClickListener { val intent = Intent(this, EnterMobileNumber::class.java)
+            startActivity(intent) }
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
---
+++ app/src/main/java/com/techad/calltranslatorapp/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.techad.calltranslatorapp
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        val button1 = findViewById<View>(R.id.button1)
+
+        button1.setOnClickListener { val intent = Intent(this, MainActivity2::class.java)
+            startActivity(intent) }
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
